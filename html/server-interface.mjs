9620dbe35a8815ff558e4810e338c20ce3d194c0
Matthew Larsen <matt4.0larsen@gmail.com>
2025-11-01 13:08:47 -0600

    add a default value for content

--- html/server-interface.mjs
+++ html/server-interface.mjs
@@ -15,7 +15,7 @@ export default class {
     }
 
     sendEvent(messageType, content) {
-        let message = content;
+        let message = content ?? {};
         message.header = {
             from: this.#playerId,
             type: messageType,
